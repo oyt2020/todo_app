ee14666a94d023942d6503739ebbb1eb87465ade
oytUser <oyt201@naver.com>
2026-01-20 17:44:41 +0900

    Todo 서비스 작성 중

--- src/main/java/com/google/todo/service/todo/TodoService.java
+++ src/main/java/com/google/todo/service/todo/TodoService.java
@@ -1,4 +1,21 @@
 package com.google.todo.service.todo;
 
+
+import com.google.todo.domain.todo.Todo;
+import com.google.todo.domain.todo.TodoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class TodoService {
+    private final TodoRepository todoRepository;
+
+    // 할 일 생성
+    public Long createTodo(String title){
+        Todo todo = Todo.create(title);
+        return todoRepository.save(todo).getId();
+    }
 }
