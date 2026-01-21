2238f6660318a60e544ab07e50ebbaab469dff1f
oytUser <oyt201@naver.com>
2026-01-21 10:32:53 +0900

    캐싱 확인

--- src/test/java/com/google/todo/service/todo/TodoServiceTest.java
+++ src/test/java/com/google/todo/service/todo/TodoServiceTest.java
@@ -1,7 +1,42 @@
 package com.google.todo.service.todo;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.google.todo.domain.todo.Todo;
+import com.google.todo.domain.todo.TodoRepository;
+import com.google.todo.domain.todo.TodoStatus;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
 class TodoServiceTest {
+
+    @Mock
+    TodoRepository todoRepository;
+
+    @InjectMocks
+    TodoService todoService;
+
+
+    @DisplayName("할 일 생성")
+    void create_todo(){
+        Todo todo = Todo.create("CI/CD 공부");
+
+        given(todoRepository.save(todo)).willReturn(todo);
+
+        Long id = todoService.createTodo("CI/CD 공부");
+
+        assertThat(id).isNull();
+        verify(todoRepository).save(todo);
+    }
 
 }
