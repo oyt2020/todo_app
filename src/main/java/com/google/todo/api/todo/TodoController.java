08af217c325cd848cce10d54ee8527b47dc07b9b
oytUser <oyt201@naver.com>
2026-01-23 09:28:50 +0900

    controller

--- src/main/java/com/google/todo/api/todo/TodoController.java
+++ src/main/java/com/google/todo/api/todo/TodoController.java
@@ -1,4 +1,66 @@
 package com.google.todo.api.todo;
 
+
+import com.google.todo.api.todo.request.TodoCreateRequest;
+import com.google.todo.api.todo.request.TodoUpdateRequest;
+import com.google.todo.api.todo.response.TodoResponse;
+import com.google.todo.domain.todo.Todo;
+import com.google.todo.domain.todo.TodoStatus;
+import com.google.todo.service.todo.TodoService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/todos")
 public class TodoController {
+
+    private final TodoService todoService;
+
+    @PostMapping
+    public Long create(@RequestBody @Valid TodoCreateRequest request) {
+        return todoService.createTodo(request.getTitle());
+    }
+
+    @GetMapping("/{id}")
+    public TodoResponse get(@PathVariable Long id) {
+        Todo todo = todoService.getTodo(id);
+        return new TodoResponse(todo);
+    }
+
+    @GetMapping
+    public List<TodoResponse> getTodos(
+            @RequestParam(required = false)TodoStatus status
+    ){
+
+        List<Todo> todos = (status == null)
+                ? todoService.getTodos()
+                : todoService.getTodosByStatus(status);
+
+        return todos.stream()
+                .map(TodoResponse::new)
+                .toList();
+    }
+
+    @PutMapping("/{id}")
+    public void update(
+            @PathVariable Long id,
+            @RequestBody @Valid TodoUpdateRequest request
+    ){
+        todoService.updateTodo(id, request.getTitle());
+    }
+
+    @PatchMapping("/{id}/complete")
+    public void complete(@PathVariable Long id){
+        todoService.completeTodo(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+        todoService.deleteTodo(id);
+    }
+
 }
--- src/main/java/com/google/todo/api/todo/response/TodoResponse.java
+++ src/main/java/com/google/todo/api/todo/response/TodoResponse.java
@@ -1,4 +1,20 @@
 package com.google.todo.api.todo.response;
 
+import com.google.todo.domain.todo.Todo;
+import com.google.todo.domain.todo.TodoStatus;
+import lombok.Getter;
+
+@Getter
 public class TodoResponse {
+
+
+    private final Long id;
+    private final String title;
+    private final TodoStatus status;
+
+    public  TodoResponse(Todo todo) {
+        this.id = todo.getId();
+        this.title = todo.getTitle();
+        this.status = todo.getStatus();
+    }
 }
