39f3c22ca152b13c50b1702ad5bfd2898ab9af41
oytUser <oyt201@naver.com>
2026-01-26 17:37:25 +0900

    docker off

--- src/test/java/com/google/todo/TestTodoApplication.java
+++ src/test/java/com/google/todo/TestTodoApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.SpringApplication;
 public class TestTodoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.from(TodoApplication::main).with(TestcontainersConfiguration.class).run(args);
+//        SpringApplication.from(TodoApplication::main).with(TestcontainersConfiguration.class).run(args);
+        SpringApplication.run(TestTodoApplication.class, args);
     }
 
 }
--- src/test/java/com/google/todo/api/todo/TodoControllerTest.java
+++ src/test/java/com/google/todo/api/todo/TodoControllerTest.java
@@ -2,6 +2,7 @@ package com.google.todo.api.todo;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class TodoControllerTest {
 
 }
