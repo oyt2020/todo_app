2bbe6598c18bf3aeb0d04eec9adaa680770ee5a4
oytUser <oyt201@naver.com>
2026-02-01 00:12:02 +0900

    cors ì임시 설정

--- src/main/java/com/google/todo/api/todo/TodoController.java
+++ src/main/java/com/google/todo/api/todo/TodoController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://35.193.65.32") // 프론트 주소
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/todos")
