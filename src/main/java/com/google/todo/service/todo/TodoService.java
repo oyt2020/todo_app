98d9c6a213fd8755953fb616407e87b4cc72f8b5
oytUser <oyt201@naver.com>
2026-02-02 11:42:04 +0900

    ídb 반영 확인

--- src/main/java/com/google/todo/service/todo/TodoService.java
+++ src/main/java/com/google/todo/service/todo/TodoService.java
@@ -44,8 +44,12 @@ public class TodoService {
     // 제목 업데이트
     @Transactional
     public void updateTodo(Long todoId, String title){
-        Todo todo = getTodo(todoId);
+
+        Todo todo = todoRepository.findById(todoId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 Todo가 없습니다."));
         todo.updateTitle(title);
+        // 확인 용
+        todoRepository.save(todo);
     }
 
     // 완료 상태 변경
