51ad6976ec5c205693b1b0a300675c9a741ed228
oytUser <oyt201@naver.com>
2026-02-02 09:37:24 +0900

    ip 주소

--- src/main/java/com/google/todo/api/todo/TodoController.java
+++ src/main/java/com/google/todo/api/todo/TodoController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://35.193.65.32") // 프론트 주소
+@CrossOrigin(origins = "http://34.58.208.188") // 프론트 주소
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/todos")
